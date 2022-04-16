975122ddb492c22bd349edc309fa514696a1e9ef
salmancz <salman.jsdev@gmail.com>
2022-04-16 17:10:18 +0530

    edited

--- flask_main.py
+++ flask_main.py
@@ -11,4 +11,4 @@ def output():
     return render_template("status.html")
 
 if __name__ =="__main__":
-    app.run(debug=False,host='0.0.0.0') 
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get("PORT",  8080))) 
